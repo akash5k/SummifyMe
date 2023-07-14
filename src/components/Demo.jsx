6af0b2a3bb1c23043a6408a04550219888841821
akash5k <akashparida185@gmai.comk>
2023-07-14 16:22:36 +0530

    hero component added

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,8 +1,139 @@
 import React from 'react'
 
+import { useState, useEffect } from 'react'
+import {copy ,linkIcon,loader,tick} from '../assets'
+import {useLazyGetSummaryQuery} from '../services/article';
+
 const Demo = () => {
+
+const[article, setArticle] = useState({
+  url:'',
+  summary:'',
+});
+
+const [allArticles , setAllArticles] = useState([]);
+
+const[copied,setCopied] = useState("")
+
+
+const [getSummary,{error, isFetching}] = useLazyGetSummaryQuery();
+
+
+// link history from local browser history
+useEffect(()=>{
+  const articlesFromLocalStorage = JSON.parse(
+    localStorage.getItem('articles')
+  )
+
+  if(articlesFromLocalStorage){
+    setAllArticles(articlesFromLocalStorage)
+  }
+},[])
+
+
+const handleSubmit = async(e)=>{
+  e.preventDefault();
+
+  const {data} = await getSummary({articleUrl: article.url});
+
+  if (data?.summary){
+    const newArticle = { ...article,summary: data.summary};
+
+    // ... -> to spreed all articles ,i.e to add the new articles at the end of existing ones (to append)   
+    const updatedAllArticle = [newArticle,...allArticles];
+     
+    setArticle(newArticle);
+    setAllArticles(updatedAllArticle);
+    
+    localStorage.setItem('articles',JSON.stringify(updatedAllArticle))
+    //local storage can only store strings
+  }
+}
+
+const handleCopy = (copyUrl) =>{
+  setCopied(copyUrl);
+  navigator.clipboard.writeText(copyUrl);
+  setTimeout(()=>setCopied(false), 3000)
+}
+
   return (
-    <div>Demo</div>
+    <section className='mt-16 w-full max-w-xl'>
+    {/* search  */}
+    <div className="flex flex-col w-full gap-2">
+    <form
+     className='relative flex justify-center items-center'
+     onSubmit={handleSubmit}
+     >
+
+     <img src={ linkIcon } alt="link_icon" 
+     className='absolute left-0 my-2 ml-3 w-5'/>
+
+     <input type="url" placeholder='Enter URL'
+      value={article.url} onChange={(e)=>setArticle({...article, url: e.target.value})}  
+      required className='url_input peer'/>
+
+      <button
+          type='submit'
+          className='submit_btn
+           peer-focus:border-gray-700
+           peer-focus:text-gray-700'>
+          ⌘
+          {/* ↩ */}
+      </button>
+    </form>
+
+    {/* Browser url history */}
+
+    <div className="flex flex-col gap-1 max-h-60-overflow-y-auto">
+
+      {allArticles.map((item,index) =>(
+        <div
+         key = {`link-${index}`}
+         onClick={()=>setArticle(item)}
+         className='link_card'
+         >
+         <div className='copy_btn' onClick={()=> handleCopy(item.url)}>
+          <img src={ copied === item.url ? tick : copy } //change the copy icon if copied
+            alt="copy_icon"
+            className='w-[40%] h-[40%] object-contain'
+          />
+         </div>
+         <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
+          {item.url}
+         </p>
+        </div>
+      ))}
+    </div>
+    </div>
+
+    {/* Display Results */}
+    <div className='my-10 max-w-full flex justify-center items-center'>
+        {isFetching ?(
+          <img src={loader} alt="loader" className='w-10 h-10' />
+        ): error ? (
+          <p className='font-inter font-bold text-black text-center'>
+            Well,that wasn't supposed to happen..
+            <br />
+            <span className='font-satoshi font-normal'>
+              {error?.data?.error}
+            </span>
+          </p>
+        ):(
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+                Article <span className='blue_gradient'>Summary</span> 
+              </h2>
+              <div className="summary_box">
+                <p className='font-inter font-medium text-sm text-gray-700'>
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
+    </div>
+    </section>
   )
 }
 
